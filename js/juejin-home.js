76ed2b01063ac903078752d4b8b72c41cad3327f
linhieng <790728740@qq.com>
2023-08-30 07:50:05 +0800

    style: 使用 prettier

--- js/juejin-home.js
+++ js/juejin-home.js
@@ -1,15 +1,15 @@
-const eleTopUl = document.querySelector('.nav-list .right-side-nav')
+const eleTopUl = document.querySelector(".nav-list .right-side-nav");
 
-const eleGotoPin = document.createElement('li')
-eleGotoPin.classList.add('nav-item', 'linhieng')
-eleGotoPin.innerHTML = ` <a target="_blank" href="https://juejin.cn/pins">发沸点</a> `
+const eleGotoPin = document.createElement("li");
+eleGotoPin.classList.add("nav-item", "linhieng");
+eleGotoPin.innerHTML = ` <a target="_blank" href="https://juejin.cn/pins">发沸点</a> `;
 
-const eleGotoCode = document.createElement('li')
-eleGotoCode.classList.add('nav-item', 'linhieng')
-eleGotoCode.innerHTML = ` <a target="_blank" href="https://code.juejin.cn/">写代码</a> `
+const eleGotoCode = document.createElement("li");
+eleGotoCode.classList.add("nav-item", "linhieng");
+eleGotoCode.innerHTML = ` <a target="_blank" href="https://code.juejin.cn/">写代码</a> `;
 
-const eleGotoDraft = document.createElement('li')
-eleGotoDraft.classList.add('nav-item', 'linhieng')
-eleGotoDraft.innerHTML = ` <a target="_blank" href="https://juejin.cn/editor/drafts/7269743983189786660">文章草稿</a> `
+const eleGotoDraft = document.createElement("li");
+eleGotoDraft.classList.add("nav-item", "linhieng");
+eleGotoDraft.innerHTML = ` <a target="_blank" href="https://juejin.cn/editor/drafts/7269743983189786660">文章草稿</a> `;
 
-eleTopUl.prepend(eleGotoDraft, eleGotoCode, eleGotoPin)
+eleTopUl.prepend(eleGotoDraft, eleGotoCode, eleGotoPin);
