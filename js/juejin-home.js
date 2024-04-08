fc4682d5911d08f2760133a74267baaae5d26ae5
linhieng <790728740@qq.com>
2024-04-08 09:52:21 +0800

    fix: 解决网页通过前进或后退进入页面时插件不生效问题

--- js/juejin-home.js
+++ js/juejin-home.js
@@ -1,15 +1,32 @@
-const eleTopUl = document.querySelector(".nav-list .right-side-nav");
+renderElementToPage()
 
-const eleGotoPin = document.createElement("li");
-eleGotoPin.classList.add("nav-item", "linhieng");
-eleGotoPin.innerHTML = ` <a target="_blank" href="https://juejin.cn/pins">发沸点</a> `;
+// 监听浏览器前进回退
+window.addEventListener('popstate', (evt) => {
+    console.log(evt)
+    console.log('你好？');
+    renderElementToPage()
+}, false)
 
-const eleGotoCode = document.createElement("li");
-eleGotoCode.classList.add("nav-item", "linhieng");
-eleGotoCode.innerHTML = ` <a target="_blank" href="https://code.juejin.cn/">写代码</a> `;
+function renderElementToPage() {
+    if (!(
+        location.origin === 'https://juejin.cn' && location.pathname === '/creator/home'
+    )) {
+        return
+    }
 
-const eleGotoDraft = document.createElement("li");
-eleGotoDraft.classList.add("nav-item", "linhieng");
-eleGotoDraft.innerHTML = ` <a target="_blank" href="https://juejin.cn/editor/drafts/7269743983189786660">文章草稿</a> `;
+    const eleTopUl = document.querySelector(".nav-list .right-side-nav");
 
-eleTopUl.prepend(eleGotoDraft, eleGotoCode, eleGotoPin);
+    const eleGotoPin = document.createElement("li");
+    eleGotoPin.classList.add("nav-item", "linhieng");
+    eleGotoPin.innerHTML = ` <a target="_blank" href="https://juejin.cn/pins">发沸点</a> `;
+
+    const eleGotoCode = document.createElement("li");
+    eleGotoCode.classList.add("nav-item", "linhieng");
+    eleGotoCode.innerHTML = ` <a target="_blank" href="https://code.juejin.cn/">写代码</a> `;
+
+    const eleGotoDraft = document.createElement("li");
+    eleGotoDraft.classList.add("nav-item", "linhieng");
+    eleGotoDraft.innerHTML = ` <a target="_blank" href="https://juejin.cn/editor/drafts/7269743983189786660">文章草稿</a> `;
+
+    eleTopUl.prepend(eleGotoDraft, eleGotoCode, eleGotoPin);
+}
